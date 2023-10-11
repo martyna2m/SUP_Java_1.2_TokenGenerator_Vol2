306c0a2589f74ca1d2fb3aa79a3d37cb20674255
martyna2m <bsom2m@gmail.com>
2023-10-11 16:19:32 +0200

    Create code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> tokenOptions = Arrays.asList(5, 10, 15);
 
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        TokenGenerator tokenGenerator = new TokenGenerator();
+
+
+        try {
+            TokenGenerator.printInstruction(tokenOptions);
+            int charNumber = scan.nextInt();
+            switch (charNumber) {
+                case 5 -> tokenGenerator.generateToken(5);
+                case 10 -> tokenGenerator.generateToken(10);
+                case 15 -> tokenGenerator.generateToken(15);
+                default -> TokenGenerator.printAlert(tokenOptions);
+            }
+        } catch (Exception e) {
+            TokenGenerator.printAlert2();
+        }
+
     }
 }
---
+++ src/main/java/org/example/TokenGenerator.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Random;
+
+public class TokenGenerator {
+
+
+    Random rand = new Random();
+
+
+    public void generateToken(int charNumber) throws InputMismatchException {
+
+        ArrayList<Character> symbols = new ArrayList<>();
+
+        for (int i = 0; i < charNumber; i++) {
+            int intForChar = rand.nextInt(33, 123);
+            while ((intForChar == 34) || (intForChar == 39) || (intForChar > 42 && intForChar < 48) || (intForChar > 57 && intForChar < 64) ||
+                    (intForChar > 90 && intForChar < 94) || (intForChar > 94 && intForChar < 97)) {
+                intForChar = rand.nextInt(33, 123);
+            }
+
+            Character symbol = (char) intForChar;
+            symbols.add(symbol);
+        }
+        String token = convertArrayToString(symbols);
+        System.out.println("Your token: " + token);
+
+    }
+
+
+    public static String convertArrayToString(ArrayList<Character> listOfChars) {
+        String token = "";
+        for (char character : listOfChars) {
+            token = token + character;
+        }
+        return token;
+    }
+
+
+    public static void printInstruction(List<Integer> options) {
+        System.out.println("Choose the number of characters in your token: " + options);
+    }
+
+
+    public static void printAlert(List<Integer> options) {
+        System.out.println("You can only choose a number from:  " + options + ", please start again. ");
+    }
+
+    public static void printAlert2() {
+        System.out.println("You can only choose a number, please start again.");
+    }
+
+
+}
